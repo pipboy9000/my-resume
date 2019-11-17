678f07769f1468638e80b9645b0fc3d738757235
Dan <dan784@gmail.com>
2019-11-17 19:24:38 +0200

    i think it's finished now

--- public/script.js
+++ public/script.js
@@ -124,3 +124,10 @@ function sendFeedback() {
         }
     })
 }
+
+function showInfo() {
+    document.getElementById('info').style.display = 'flex'
+}
+function hideInfo() {
+    document.getElementById('info').style.display = 'none'
+}
