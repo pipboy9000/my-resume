8d78deb77e1f4f1d8bb2e562ad50e3ee2be21b0f
Dan Levin <dan784@gmail.com>
2024-06-22 14:15:09 +0300

    fixes and updates

--- medias.js
+++ medias.js
@@ -7,9 +7,7 @@ function getStackOverflowData(uid) {
         let $ = cheerio.load(res.data);
         let obj = {};
         obj.badges = {};
-
         obj.reputation = $('.grid--cell.fs-title.fc-dark').text();
-
         obj.badges.bronze = $('[title*="bronze badges"]').text().trim();
         obj.badges.silver = $('[title*="silver badges"]').text().trim();
         obj.badges.gold = $('[title*="gold badges"]').text().trim();
